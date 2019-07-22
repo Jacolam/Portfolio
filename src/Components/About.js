0d2f086fe386ec2eea23974bdb190ddc92d54ffa
Jacob Lam <jlam18@binghamton.edu>
2019-07-22 17:32:46 -0400

    added about me, still need revisions

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,7 +4,11 @@ class About extends React.Component{
   render(){
     return(
       <div className='About'>
-        About Section
+        About Me<br/>
+      Full stack web developer with experience in JavaScript, React/Redux, Ruby on Rails and a background in engineering. I have always had a passion for problem solving, building, and technology. Working at Tesla spurred my interest in software development, helping me realize that software development linked my three passiosn together. Which has lead me to Flatiron Schoolm where iam now embarking on my journey as a software developer.
+
+      When I'm not coding, you'll find me eating, cooking, eating what I cooked, working out, and playing some videogames.
+      
       </div>
     )
   }
