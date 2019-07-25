72a6819b5eece894e2143f3ba5d9496b112e0ec6
Jacob Lam <jlam18@binghamton.edu>
2019-07-25 12:03:10 -0400

    updated bio

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,7 +5,7 @@ class About extends React.Component{
     return(
       <div className='About content-block container' style={{background:'gray'}}>
         About Me<br/>
-      Full-stack web developer with experience in JavaScript, React/Redux, Ruby on Rails and engineering background. I have always had a passion for problem-solving, building, and technology. Working at Tesla spurred my interest in software development, helping me realize that software development linked my three passiosn together. Which has lead me to Flatiron Schoolm where iam now embarking on my journey as a software developer.
+      I am full-stack web developer with experience in JavaScript, React/Redux, Ruby on Rails and an engineer background. Having a chronically curious mind, I fostered  passions for solution development, troubleshooting, and technological innovation. This fascination was further spurred by my time working at Tesla, helping me realize that software melded my three passions together. I enrolled to Flatiron School to embark on my journey as a software developer. 
         <br/>
         When I'm not coding, you'll find me eating, cooking, eating what I cooked, working out, and playing some videogames.
       </div>
