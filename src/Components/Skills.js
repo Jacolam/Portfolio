725c6f5ca292ac3f61e0231f022f5099757caa38
Jacob Lam <jlam18@binghamton.edu>
2019-08-01 13:28:14 -0400

    finished adding skills logos

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -4,13 +4,6 @@ class Skills extends React.Component{
   render(){
     return(
       <div className='Skills bar'>
-        Skills Section
-        <img src=''/>
-
-        Ruby on Rails
-        React
-        Redux
-        Ruby
         CSS
         PostgreSQL
 
@@ -29,8 +22,32 @@ class Skills extends React.Component{
         <img
           src='https://banner2.kisspng.com/20180824/ghg/kisspng-javascript-clip-art-openclipart-logo-number-5b8045d0be7b50.1522899915351331367802.jpg'
           alt="JavaScript"
+          height="45"
+          width="45"
+        />
+        <img
+          src='https://cdn.freebiesupply.com/logos/large/2x/redux-logo-png-transparent.png'
+          alt="Redux"
+          height="45"
+          width="45"
+        />
+        <img
+          src='https://arcweb.co/wp-content/uploads/2016/10/react-logo-1000-transparent.png'
+          alt="React"
           height="50"
           width="50"
+        />
+        <img
+          src='https://i.dlpng.com/static/png/303333_preview.png'
+          alt="Ruby"
+          height="50"
+          width="50"
+        />
+        <img
+          src='https://cdn.freebiesupply.com/logos/thumbs/2x/rails-1-logo.png'
+          alt="Ruby on Rails"
+          height="100"
+          width="100"
         />
 
       </div>
